988d06fe55d2a6ed22b4666f375ef46cd26a7d1b
angelAntezana <angel2estevan@gmail.com>
2023-09-06 22:51:01 +0200

    My first commit

---
+++ email-verification-demo/src/main/java/com/angel/antezana/emailverificationdemo/EmailVerificationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.angel.antezana.emailverificationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVerificationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailVerificationDemoApplication.class, args);
+	}
+
+}
