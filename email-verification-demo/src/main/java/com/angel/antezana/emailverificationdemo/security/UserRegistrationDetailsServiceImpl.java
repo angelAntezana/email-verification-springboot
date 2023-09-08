cbfd2f891be08ca640d00fa5eaae5a59baeda1e3
angelAntezana <angel2estevan@gmail.com>
2023-09-08 13:17:47 +0200

    UserRegistrationDetails implementación de UserDetails

---
+++ email-verification-demo/src/main/java/com/angel/antezana/emailverificationdemo/security/UserRegistrationDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.angel.antezana.emailverificationdemo.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.angel.antezana.emailverificationdemo.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserRegistrationDetailsServiceImpl implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+       
+        return userRepository.findByEmail(email)
+        .map(UserRegistrationDetails::new)
+        .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+    
+}
